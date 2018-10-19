5203635dc53027bdee485651ff99fbfe888a9f6b
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2018-10-19 01:44:17 +0100

    Edited the Login.js and NewMember.js

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,54 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import avatar from "../../img/avatar.svg";
+import axios from "axios";
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reg_no: "",
+      password: "",
+      login: {},
+      error: {}
+    };
+  }
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  Login = login => {
+    axios
+      .post(
+        "http://localhost:8080/projects/sanctuary/server/public/login",
+        login
+      )
+      .then(result => {
+        this.setState({ login: result.data });
+        console.log(this.state);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  submit = e => {
+    e.preventDefault();
+    if (this.state.reg_no === "" || this.state.password === "") {
+      const err = {
+        reg_no: "Registration Number Required",
+        password: "Password Required"
+      };
+      this.setState({ error: err });
+      console.log(this.state.error);
+    } else {
+      const login = {
+        reg_no: this.state.reg_no,
+        password: this.state.password
+      };
+      this.Login(login);
+    }
+  };
   render() {
     return (
       <div>
@@ -21,24 +68,39 @@ class Login extends Component {
                 />
               </div>
               <div className="card-content">
-                <form action="">
+                <form action="" onSubmit={this.submit}>
                   <div className="input-field">
                     <i className="material-icons prefix">assignment_ind</i>
                     <label htmlFor="RegNo">Enter Reg Number:</label>
-                    <input type="number" id="RegNo" className="validate" />
+                    <input
+                      type="number"
+                      id="RegNo"
+                      className="validate"
+                      name="reg_no"
+                      value={this.state.reg_no}
+                      onChange={this.onChange}
+                    />
                   </div>
                   <div className="input-field">
                     <i className="material-icons prefix"> lock_outline</i>
                     <label htmlFor="password" className="active">
                       Enter Password:
                     </label>
-                    <input type="text" id="password" className="validate" />
+                    <input
+                      type="password"
+                      id="password"
+                      className="validate"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.onChange}
+                    />
                   </div>
-                  <input
+                  <button
                     type="submit"
                     className="btn waves-effect waves-light green"
-                    value="Send"
-                  />
+                  >
+                    Send
+                  </button>
                 </form>
               </div>
             </div>
